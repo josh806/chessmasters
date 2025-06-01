43e32929de3810fc422fc198adfeafe6f2cf3105
Josh McCarthy <joshcaterham@gmail.com>
2025-06-01 21:10:13 +0200

    Setup tests

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'node:path';
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: path.resolve(__dirname, 'src/setupTests.ts'),
+  },
+});
